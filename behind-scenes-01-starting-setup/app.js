7c253cb748fed44fe55fe515339d81862c15fcb3
Michelle <michelle.lefebvre@valtech.com>
2022-01-19 10:26:44 -0500

    using vue.cli project code by instructor

---
+++ behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,32 @@
+const data = {
+  message: 'Hello!'
+};
+
+const handler = {
+  set(target, key, value) {
+    console.log(target);
+    console.log(key);
+    console.log(value);
+  }
+};
+
+const proxy = new Proxy(data, handler);
+
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: '',
+      message: 'Vue is great!'
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      this.message = this.currentUserInput;
+    }
+  }
+});
+
+app.mount('#app');
