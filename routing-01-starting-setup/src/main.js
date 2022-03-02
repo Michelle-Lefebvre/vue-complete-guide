3669928378f80930dd2b2296b5340b31757387a0
Michelle <michelle.lefebvre@valtech.com>
2022-03-02 17:07:25 -0500

    used nested routes

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -13,11 +13,12 @@ const router = createRouter({
   routes: [
     { path: '/', redirect: '/teams' }, // NOTE: redirect changes the url while alias does not.
     // { path: '/teams', component: TeamsList, alias: '/' },
-    { path: '/teams', component: TeamsList },
+    {
+      path: '/teams',
+      component: TeamsList,
+      children: [{ path: ':teamId', component: TeamMembers, props: true }],
+    },
     { path: '/users', component: UsersList },
-    // { path: '/teams/new/' },
-    // { path: '/teams/:teamId' }, // order matters
-    { path: '/teams/:teamId', component: TeamMembers, props: true },
     { path: '/:notFound(.*)', component: NotFound }, // this should be the last past so it doesn't override any other path
   ],
   linkActiveClass: 'active',
