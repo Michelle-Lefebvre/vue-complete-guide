b208d62cd50aa62a9de0d4d829af78078698742a
Michelle <michelle.lefebvre@valtech.com>
2022-03-02 06:34:55 -0500

    add components: base-card, base-button, header

---
+++ prj-cmp-01-starting-setup/src/components/layouts/TheHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+
+export default {
+  props: ['title'],
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+header {
+  width: 100%;
+  height: 5rem;
+  background-color: #640032;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  color: white;
+}
+</style>
--- prj-cmp-01-starting-setup/src/components/resources/LearningResource.vue
+++ prj-cmp-01-starting-setup/src/components/resources/LearningResource.vue
@@ -1,15 +1,15 @@
 <template>
   <li>
-    <div>
+    <base-card>
       <header>
         <h3>{{ title }}</h3>
-        <button class="btn">Delete</button>
+        <base-button btnText="x"></base-button>
       </header>
-    </div>
-    <p>{{ description }}</p>
-    <nav>
-      <a :href="link">View Resource</a>
-    </nav>
+      <p>{{ description }}</p>
+      <nav>
+        <a :href="link">View Resource</a>
+      </nav>
+    </base-card>
   </li>
 </template>
 
@@ -48,30 +48,5 @@ a {
 a:hover,
 a:active {
   color: #c89300;
-}
-/* button */
-
-.btn {
-  box-shadow: 3px 4px 0px 0px #8a2a21;
-  background: linear-gradient(to bottom, #c62d1f 5%, #f24437 100%);
-  background-color: #c62d1f;
-  border-radius: 18px;
-  border: 1px solid #d02718;
-  display: inline-block;
-  cursor: pointer;
-  color: #ffffff;
-  font-family: Arial;
-  font-size: 17px;
-  padding: 7px 25px;
-  text-decoration: none;
-  text-shadow: 0px 1px 0px #810e05;
-}
-.btn:hover {
-  background: linear-gradient(to bottom, #f24437 5%, #c62d1f 100%);
-  background-color: #f24437;
-}
-.btn:active {
-  position: relative;
-  top: 1px;
 }
 </style>
