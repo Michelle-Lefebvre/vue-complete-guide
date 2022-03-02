b7f7d8bab619e5cbc08592ab73e936576e899f96
Michelle <michelle.lefebvre@valtech.com>
2022-03-02 16:18:45 -0500

    urls: redirect, alias, catch all wrong urls to paage not found

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -5,16 +5,20 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
-  // register routes. name the urls based on where the user is
+  // register routes. Name the urls based on where the user is.
   routes: [
+    { path: '/', redirect: '/teams' }, // NOTE: redirect changes the url while alias does not.
+    // { path: '/teams', component: TeamsList, alias: '/' },
     { path: '/teams', component: TeamsList },
     { path: '/users', component: UsersList },
     // { path: '/teams/new/' },
     // { path: '/teams/:teamId' }, // order matters
     { path: '/teams/:teamId', component: TeamMembers, props: true },
+    { path: '/:notFound(.*)', component: NotFound }, // this should be the last past so it doesn't override any other path
   ],
   linkActiveClass: 'active',
 });
