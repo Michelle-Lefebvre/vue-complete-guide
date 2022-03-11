fb257341f968f46eb4577d4d0c97025791abd17f
Michelle <michelle.lefebvre@valtech.com>
2022-03-11 09:46:28 -0500

    create form to register a coach

--- main-prj-01-starting-setup/src/pages/coaches/CoachRegistration.vue
+++ main-prj-01-starting-setup/src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,17 @@
 <template>
-  <h2>Coach Registration</h2>
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue'
+export default {
+  components: {
+    CoachForm,
+  }
+}
+</script>
